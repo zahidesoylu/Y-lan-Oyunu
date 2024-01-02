72045a04237c1faa016f38bfd5729b212baa6eb6
zahidesoylu <120675021+zahidesoylu@users.noreply.github.com>
2024-01-02 15:20:47 +0300

    initial commit
    first commit

---
+++ YılanOyunu/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YılanOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        yilanoyunu yilanim;
+        yonu yonum;
+        PictureBox[] pcyilanparcalari;
+        bool yemkontrol = false;
+        PictureBox pcyem;
+        Random rastgele = new Random();
+        int skor = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yenioyun();
+
+        }
+
+
+
+        private void yenioyun()
+        {
+            yilanim = new yilanoyunu();
+            yonum = new yonu(-10, 0);
+            pcyilanparcalari = new PictureBox[0];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Array.Resize(ref pcyilanparcalari, pcyilanparcalari.Length + 1);
+                pcyilanparcalari[i] = pcekle();
+            }
+            button1.Enabled = false;
+            timer1.Start();
+
+
+        }
+
+        private PictureBox pcekle()
+        {
+            PictureBox pc = new PictureBox();
+            pc.Size = new Size(10, 10);
+            pc.BackColor = Color.Blue;
+            pc.Location = yilanim.pozisyon(pcyilanparcalari.Length - 1);
+            panel1.Controls.Add(pc);
+            return pc;
+        }
+
+        private void guncelle()
+        {
+            for (int i = 0; i < pcyilanparcalari.Length; i++)
+            {
+                pcyilanparcalari[i].Location = yilanim.pozisyon(i);
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                if(yonum.yon_y != 10)
+                {
+                    yonum = new yonu(0, -10);
+                }
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (yonum.yon_y != -10)
+                {
+                    yonum = new yonu(0, 10);
+                }
+            }
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (yonum.yon_x != 10)
+                {
+                    yonum = new yonu(-10, 0);
+                }
+            }
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (yonum.yon_x != -10)
+                {
+                    yonum = new yonu(10, 0);
+                }
+            }
+        }
+
+        public void yem()
+        {
+            if (!yemkontrol)
+            {
+                PictureBox pc = new PictureBox();
+                pc.BackColor = Color.Red;
+                pc.Size = new Size(10,10);
+                pc.Location = new Point(rastgele.Next(panel1.Width/10)*10, rastgele.Next(panel1.Height/10) * 10);
+                pcyem = pc;
+                yemkontrol = true;
+                panel1.Controls.Add(pc);
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Skor:" + skor.ToString();
+            yilanim.yilanilerle(yonum);
+            guncelle();
+            yem();
+            yemiye();
+            duvaracarpti();
+        }
+
+        public void yemiye()
+        {
+            if(yilanim.pozisyon(0) == pcyem.Location)
+            {
+                skor += 10;
+                yilanim.yilanboyu();
+                Array.Resize(ref pcyilanparcalari,pcyilanparcalari.Length+1);
+                pcyilanparcalari[pcyilanparcalari.Length - 1] = pcekle();
+                yemkontrol=false;
+                panel1.Controls.Remove(pcyem);
+            }
+        }
+
+        public void duvaracarpti()
+        {
+            Point pz = yilanim.pozisyon(0);
+            if(pz.X < 0 || pz.X> panel1.Width-10 || pz.Y > panel1.Width - 10)
+            {
+                timer1.Stop();
+                MessageBox.Show("Oyun bitti! KAYBETTİN...");
+                button1.Enabled = true;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_FontChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            yenioyun();
+
+        }
+    }
+}
---
+++ YılanOyunu/yilanoyunu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YılanOyunu
+{
+    internal class yilanoyunu
+    {
+        yilanparcalari[] yilanparcasi;
+        int buyukluk;
+        yonu yonum;
+
+        public yilanoyunu()
+        {
+            yilanparcasi = new yilanparcalari[3];
+            buyukluk = 3;
+            yilanparcasi[0] = new yilanparcalari(150, 150);
+            yilanparcasi[1] = new yilanparcalari(160, 150);
+            yilanparcasi[2] = new yilanparcalari(170, 150);
+
+
+        }
+
+        public void yilanilerle(yonu yon)
+        {
+            yonum = yon;
+
+            if(yon.yon_x == 0 && yon.yon_y == 0)
+            {
+
+            }
+            else
+            {
+                if (yon != null)
+                {
+                    for (int i = yilanparcasi.Length - 1; i > 0; i--)
+                    {
+                         yilanparcasi[i] = new yilanparcalari(yilanparcasi[i - 1].parca_x, yilanparcasi[i - 1].parca_y);
+                    }
+                    yilanparcasi[0] = new yilanparcalari(yilanparcasi[0].parca_x + yon.yon_x, yilanparcasi[0].parca_y + yon.yon_y);
+                }
+            }
+        }
+
+        public void yilanboyu()
+        {
+
+            if(yilanparcasi.Length> 0)
+            {
+                Array.Resize(ref yilanparcasi, yilanparcasi.Length + 1);
+                yilanparcasi[yilanparcasi.Length - 1] = new yilanparcalari(yilanparcasi[yilanparcasi.Length - 2].parca_x - yonum.yon_x, yilanparcasi[yilanparcasi.Length - 2].parca_y - yonum.yon_y);
+                buyukluk++;
+
+            }
+            
+        }
+
+        public Point pozisyon(int numara)
+        {
+            return new Point(yilanparcasi[numara].parca_x, yilanparcasi[numara].parca_y);
+        }
+
+    }
+    class yilanparcalari
+    {
+        public int parca_x;
+        public int parca_y;
+
+        public readonly int size_x;
+        public readonly int size_y;
+
+
+        public yilanparcalari(int x, int y)
+        {
+            parca_x = x;
+            parca_y = y;
+            size_x = 10;
+            size_y = 10;
+
+        }
+    }
+
+    public class yonu
+    {
+        public readonly int yon_x;
+        public readonly int yon_y;
+        public yonu(int x, int y)
+        {
+            yon_x = x;
+            yon_y = y;
+        }
+    }
+}
+
